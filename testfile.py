9c81628fb31c00d7e0294fc0d375f6a0897a7076
Irina Delamare <irina.delamare@gmail.com>
2023-03-24 15:23:02 +0100

    update dashboard

--- testfile.py
+++ testfile.py
@@ -8,13 +8,8 @@ import pandas as pd
 import numpy as np
 
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-chart_data
-
-st.line_chart(chart_data)
-
 data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+
+data 
 
 print("Shape of cdata:", data.shape)
