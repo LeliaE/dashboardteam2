f0fa9ce205b6cd3ece8196d0647da37045e36bed
Irina Delamare <irina.delamare@gmail.com>
2023-03-31 15:51:40 +0200

    updated testfile, should be the last time I update this file

--- testfile.py
+++ testfile.py
@@ -10,6 +10,13 @@ import numpy as np
 
 data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 
+""" location = data[location]
+date = data[date]
+total_death = data[total_death]
+"""
 data 
+"""print("Shape of cdata:", data.shape)"""
 
-print("Shape of cdata:", data.shape)
+
+
+""" st.line_chart(data, x = date, y = total_death )"""
